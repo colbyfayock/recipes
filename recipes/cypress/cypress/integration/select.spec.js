cc4217e476e66bd48fe6d2f39ba7ad57ec65bdd2
Colby Fayock <fayock@gmail.com>
2021-02-23 16:06:22 -0500

    updating recipe for cypres selects

--- recipes/cypress/cypress/integration/select.spec.js
+++ recipes/cypress/cypress/integration/select.spec.js
@@ -4,25 +4,12 @@ describe("Select", () => {
     cy.visit('/ingredients/select');
   })
 
-  describe('Default', () => {
-
-    it(`should be the first option without a default selected value`, function () {
-      cy.get('#spices-default').should('have.value', 'chili-powder')
-    });
-
-    it(`should choose an item from a Select dropdown`, function () {
-      const value = 'paprika';
-      cy.get('#spices-default').select(value).should('have.value', value)
-    });
-
+  it('should select an option from the select dropdown', () => {
+    cy.get('#spices-select-single').select('ginger').should('have.value', 'ginger')
   });
 
-  describe('With Default Selected Value', () => {
-
-    it(`should be the first option without a default selected value`, function () {
-      cy.get('#spices-selected').should('have.value', 'ginger')
-    });
-
+  it('should select multiple options from the multi select dropdown', () => {
+    cy.get('#spices-select-multi').select(['ginger', 'paprika']).invoke('val').should('deep.equal', ['ginger', 'paprika'])
   });
 
 });
